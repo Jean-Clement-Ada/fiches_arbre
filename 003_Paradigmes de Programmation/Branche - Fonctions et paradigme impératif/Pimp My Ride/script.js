eb4b3870e27cbc71a1529f30a57ffa4a6cfab8de
Jean-Clément <jeanclement@audusseau.fr>
2023-11-07 18:30:15 +0100

    création fiche arbre 3

---
+++ 003_Paradigmes de Programmation/Branche - Fonctions et paradigme impératif/Pimp My Ride/script.js
@@ -0,0 +1,6 @@
+let tripToParse = "Perdita 8 10 8" //chaîne de caractère
+
+function parseTrip(trip) {
+tripToParse.split()
+}
+parseTrip(tripToParse)
---
+++ 003_Paradigmes de Programmation/Branche - Fonctions et paradigme impératif/Énoncé d’exercice d’écriture de code/script.js
@@ -0,0 +1,5 @@
+function petitNom() {
+reponse = prompt("quel est ton nom visiteuse")
+return reponse
+}
+alert("bonjour " + petitNom())
